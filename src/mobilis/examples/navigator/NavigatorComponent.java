ce556da2af4b589edd3bb3b1a038fab32b463bda
Hubert Fonseca <hubefonseca@gmail.com>
2009-05-05 23:59:54 -0300

    Mobilis example app v0

---
+++ src/mobilis/examples/navigator/NavigatorComponent.java
@@ -0,0 +1,61 @@
+package mobilis.examples.navigator;
+
+import mobilis.context.Context;
+import android.content.Intent;
+import android.os.RemoteException;
+import android.util.Log;
+
+
+public class NavigatorComponent extends mobilis.impl.Component {
+
+	@Override
+	public void connected(String receptacleName) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void disconnected(String receptacleName) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void registerDependencies() throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void registerReceptacles() throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void registerServices() throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void start() throws RemoteException {
+		Log.d(this.getClass().getName(), "Navigator started!");
+		
+		Intent intent = new Intent(contextWrapper, NavigatorActivity.class);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		contextWrapper.startActivity(intent);
+	}
+
+	@Override
+	public void stop() throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean canOperateUnder(Context arg0) throws RemoteException {
+		return true;
+	}
+
+}
